9e2705d05309f1646104b56a80f361810ae9fecc
mhdashmal <19mcs21@meaec.edu.in>
2024-02-25 10:54:24 +0530

    landing-page-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeaturesComponent } from './features/features.component';
+import { WorksComponent } from './works/works.component';
+import { AboutComponent } from './about/about.component';
+import { TeamsComponent } from './teams/teams.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    FeaturesComponent,
+    WorksComponent,
+    AboutComponent,
+    TeamsComponent,
+    ClientsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
